8e02e55716125b0b7db338477f06d8c54cd0dc1d
Douglas Nascimento <douglas.nascimento@GRUPOPEREIRA.COM.BR>
2018-03-27 09:02:09 -0300

    teste

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
+aaafs
 var index = require('./src/routes/index.route');
 //var users = require('./src/routes/');
 
