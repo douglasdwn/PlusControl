b035efd439ce8064bb835e0f3dd8d90558040800
douglasdwn <douglas_dwn@hotmail.com>
2018-03-28 20:35:28 -0300

    Error expressa
    Ativado midleware para error de routa

--- app.js
+++ app.js
@@ -62,23 +62,23 @@ app.use('/doc', express.static(path.join(__dirname, 'public/apidoc')));
 app.use('/api/v1', index);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   // var err = new Error('Não encontrado');
-//   // err.status = 404;
-//   // next(err);
-//   res.render('index',  { title: 'Express' });
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Não encontrado');
+  err.status = 404;
+  next(err);
+  res.render('index',  { title: 'Express' });
+});
 
-// // // error handler
-// // app.use(function(err, req, res, next) {
-// //   // set locals, only providing error in development
-// //   res.locals.message = err.message;
-// //   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// //   // render the error page
-// //   res.status(err.status || 500);
-// //   res.render('error');
-// // });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 module.exports = app;
