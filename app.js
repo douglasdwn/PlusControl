2ed44a30007325aab59057ec953c6a77087bc3c8
douglasdwn <douglas_dwn@hotmail.com>
2018-03-28 21:10:10 -0300

    Error router e index
    Error router funcionado e index

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-//homologacao
-
-
-//agora vai
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -27,17 +23,13 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
 var index = require('./src/routes/index.route');
-//var users = require('./src/routes/');
-
-//var CategoriaController = require('./src/controller/categoriaController');
 
 var app = express();
 
 var router = express.Router();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname + '/src/', 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -51,14 +43,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 //api doc
 app.use('/doc', express.static(path.join(__dirname, 'public/apidoc')));
 
-//app.use('/', index);
+//Index api
+app.get('/', function(req, res) {
+  res.render('index', {title: 'PlusControl API'});
+});
 
-//antiga implementacao
-//app.use('/api/v1', router);
-
-//var cc = new CategoriaController(router);
-//app.get('/users', users);
-
+//Api Router
 app.use('/api/v1', index);
 
 // catch 404 and forward to error handler
@@ -66,18 +56,17 @@ app.use(function(req, res, next) {
   var err = new Error('Não encontrado');
   err.status = 404;
   next(err);
-  res.render('index',  { title: 'Express' });
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  //set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{title: 'Error'});
 });
 
 
